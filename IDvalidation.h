fe88cc89e6325cd11aa5f22cecc5c5175566ffb1
Trieu Tran <114438848+trieut415@users.noreply.github.com>
2024-01-06 12:26:34 -0800

    Create IDvalidation.h

---
+++ IDvalidation.h
@@ -0,0 +1,16 @@
+#ifndef IDvalidation
+#define IDvalidation
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <cctype>
+
+using namespace std;
+
+int idCheck();
+
+#endif /* IDvalidation */
